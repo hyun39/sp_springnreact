8ef6b01f2cec9845113850d531ab2c31d7df316e
manure39 <manure39@gmail.com>
2022-03-24 00:03:36 +0900

    Calling Our Spring Boot API #5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { Component } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    clients: []
+  };
 
+  async componentDidMount() {
+    const response = await fetch('/clients');
+    const body = await response.json();
+    this.setState({clients: body});
+  }
+
+  render() {
+    const {clients} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Clients</h2>
+              {clients.map(client =>
+                  <div key={client.id}>
+                    {client.name} ({client.email})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
+}
 export default App;
