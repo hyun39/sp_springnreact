fd8f1447790a851ebcf43c8315aeb2d6ec299ff8
manure39 <manure39@gmail.com>
2022-03-24 00:41:59 +0900

    Creating an AppNavBar Component #8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,88 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React, { Component } from 'react';
-
-import { Link } from 'react-router-dom';
-
-import { Button, ButtonGroup, Container, Table } from 'reactstrap';
-
-
-import AppNavbar from './AppNavbar';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClientList from './ClientList';
+import ClientEdit from "./ClientEdit";
 
 class App extends Component {
-
-
-  state = {
-    clients: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('/clients');
-    const body = await response.json();
-    this.setState({clients: body});
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/clients' exact={true} component={ClientList}/>
+            <Route path='/clients/:id' component={ClientEdit}/>
+          </Switch>
+        </Router>
+    )
   }
-
-  async remove(id) {
-    await fetch(`/clients/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    }).then(() => {
-        let updatedClients = [...this.state.clients].filter(i => i.id !== id);
-        this.setState({clients: updatedClients});
-    });
 }
 
-
-
-render() {
-  const {clients, isLoading} = this.state;
-
-  if (isLoading) {
-      return <p>Loading...</p>;
-  }
-
-  const clientList = clients.map(client => {
-      return <tr key={client.id}>
-          <td style={{whiteSpace: 'nowrap'}}>{client.name}</td>
-          <td>{client.email}</td>
-          <td>
-              <ButtonGroup>
-                  <Button size="sm" color="primary" tag={Link} to={"/clients/" + client.id}>Edit</Button>
-                  <Button size="sm" color="danger" onClick={() => this.remove(client.id)}>Delete</Button>
-              </ButtonGroup>
-          </td>
-      </tr>
-  });
-
-  return (
-      <div>
-          <AppNavbar/>
-          <Container fluid>
-              <div className="float-right">
-                  <Button color="success" tag={Link} to="/clients/new">Add Client</Button>
-              </div>
-              <h3>Clients</h3>
-              <Table className="mt-4">
-                  <thead>
-                  <tr>
-                      <th width="30%">Name</th>
-                      <th width="30%">Email</th>
-                      <th width="40%">Actions</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {clientList}
-                  </tbody>
-              </Table>
-          </Container>
-      </div>
-  );
-}
-}
 export default App;
